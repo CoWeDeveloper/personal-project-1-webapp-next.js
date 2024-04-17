a17740c0a634c396481f157fbc2e25469af186bf
CoWeDeveloper <owaisilyas22@gmail.com>
2024-04-17 17:36:30 +0500

    86 Customer logo added till with proper naming and alt in the customer value section of customer page.tsx. Customer page.tsx is completed developed.

--- src/components/Customers/ValuableCustomers.tsx
+++ src/components/Customers/ValuableCustomers.tsx
@@ -10,11 +10,11 @@ function ValuableCustomers() {
     <section className=' w-full h-fit bg-white'>
         <div className='max-w-screen-lg mx-auto pt-20'>
             <h2 className='text-center text-4xl text-black font-black'>Our Valuable Customers</h2>
-            <div className="flex flex-wrap gap-10 px-10 pt-7 pb-10">
+            <div className="flex flex-wrap justify-center gap-14 px-10 pt-7 pb-10">
                {customersData.map((customersData)=>{
                 return(
                     <div key={customersData.id} className="flex justify-center items-center">
-                        <Image src={customersData.logo} alt={customersData.alt} width={100} height={120} />       
+                        <Image className="w-20 " src={customersData.logo} title={customersData.alt} alt={customersData.alt} width={100} height={120} />       
                     </div>
 
                 )
--- src/data/customersData.ts
+++ src/data/customersData.ts
@@ -126,93 +126,310 @@ export const customersData = [
     },
     {
         id: "26",
-        alt: "fay ",
+        alt: "mothercare",
         logo: "/assets/images/Customers/c26.png"
     },
     {
         id: "27",
-        alt: "sixB",
+        alt: "fay",
         logo: "/assets/images/Customers/c27.png"
     },
     {
         id: "28",
-        alt: "sjs",
+        alt: "sixB",
         logo: "/assets/images/Customers/c28.png"
     },
     {
         id: "29",
-        alt: "fruitNation",
+        alt: "sjs",
         logo: "/assets/images/Customers/c29.png"
     },
     {
         id: "30",
-        alt: "diamond",
+        alt: "fruitNation",
         logo: "/assets/images/Customers/c30.png"
     },
     {
         id: "31",
-        alt: "deer",
+        alt: "diamond",
         logo: "/assets/images/Customers/c31.png"
     },
     {
         id: "32",
-        alt: "chefOne",
+        alt: "deer",
         logo: "/assets/images/Customers/c32.png"
     },
     {
         id: "33",
-        alt: "rupali",
+        alt: "chefOne",
         logo: "/assets/images/Customers/c33.png"
     },
     {
         id: "34",
-        alt: "waresa",
+        alt: "rupali",
         logo: "/assets/images/Customers/c34.png"
     },
     {
         id: "35",
-        alt: "perfect",
+        alt: "waresa",
         logo: "/assets/images/Customers/c35.png"
     },
     {
         id: "36",
-        alt: "tte",
+        alt: "perfect",
         logo: "/assets/images/Customers/c36.png"
     },
     {
         id: "37",
-        alt: "tecno",
+        alt: "tte",
         logo: "/assets/images/Customers/c37.png"
     },
     {
         id: "38",
-        alt: "tpe",
+        alt: "tecno",
         logo: "/assets/images/Customers/c38.png"
     },
     {
         id: "39",
-        alt: "jtm",
+        alt: "tpe",
         logo: "/assets/images/Customers/c39.png"
     },
     {
         id: "40",
-        alt: "trax",
+        alt: "jtm",
         logo: "/assets/images/Customers/c40.png"
     },
     {
         id: "41",
-        alt: "awaisCorpation",
+        alt: "trax",
         logo: "/assets/images/Customers/c41.png"
     },
     {
         id: "42",
-        alt: "pharmaHouse",
+        alt: "awaisCorpation",
         logo: "/assets/images/Customers/c42.png"
-       },
-       {
-        id: "42",
+    },
+    {
+        id: "43",
         alt: "pharmaHouse",
-        logo: "/assets/images/Customers/c42.png"
-       },
-    
-]
+        logo: "/assets/images/Customers/c43.png"
+    }, 
+    {
+        id: "44",
+        alt: "sana",
+        logo: "/assets/images/Customers/c44.png"
+    },
+    {
+        id: "45",
+        alt: "chase",
+        logo: "/assets/images/Customers/c45.png"
+    },
+    {
+        id: "46",
+        alt: "kifayah",
+        logo: "/assets/images/Customers/c46.png"
+    },        
+    {
+        id: "47",
+        alt: "bazaarSuperMarket",
+        logo: "/assets/images/Customers/c47.png"
+    },
+    {
+        id: "48",
+        alt: "shazzSuperMarket",
+        logo: "/assets/images/Customers/c48.png"
+    },        
+    {
+        id: "49",
+        alt: "malak",
+        logo: "/assets/images/Customers/c49.png"
+    },
+    {
+        id: "50",
+        alt: "chasePlus",
+        logo: "/assets/images/Customers/c50.png"
+    },        
+    {
+        id: "51",
+        alt: "kkMart",
+        logo: "/assets/images/Customers/c51.png"
+    },
+    {
+        id: "52",
+        alt: "kuiker",
+        logo: "/assets/images/Customers/c52.png"
+    },        
+    {
+        id: "53",
+        alt: "K",
+        logo: "/assets/images/Customers/c53.png"
+    },
+    {
+        id: "54",
+        alt: "buyTime",
+        logo: "/assets/images/Customers/c54.png"
+    },        
+    {
+        id: "55",
+        alt: "bigBuy",
+        logo: "/assets/images/Customers/c55.png"
+    },
+    {
+        id: "56",
+        alt: "dawood",
+        logo: "/assets/images/Customers/c56.png"
+    },        
+    {
+        id: "57",
+        alt: "mdsSuperMarket",
+        logo: "/assets/images/Customers/c57.png"
+    },
+    {
+        id: "58",
+        alt: "magnetShoppingCity",
+        logo: "/assets/images/Customers/c58.png"
+    },        
+
+    {
+        id: "59",
+        alt: "nationalSuperMarket",
+        logo: "/assets/images/Customers/c59.png"
+    },
+    {
+        id: "60",
+        alt: " itallianMall",
+        logo: "/assets/images/Customers/c60.png"
+    },        
+    {
+        id: "61",
+        alt: "jbSaeed",
+        logo: "/assets/images/Customers/c61.png"
+    },
+    {
+        id: "62",
+        alt: "kasMeat",
+        logo: "/assets/images/Customers/c62.png"
+    },        
+    {
+        id: "63",
+        alt: "chaiChatt",
+        logo: "/assets/images/Customers/c63.png"
+    },
+    {
+        id: "64",
+        alt: "pizzaMax",
+        logo: "/assets/images/Customers/c64.png"
+    },        
+    {
+        id: "65",
+        alt: "pizza20",
+        logo: "/assets/images/Customers/c65.png"
+    },
+    {
+        id: "66",
+        alt: "biryani",
+        logo: "/assets/images/Customers/c66.png"
+    },        
+    {
+        id: "67",
+        alt: "kabir",
+        logo: "/assets/images/Customers/c67.png"
+    },
+    {
+        id: "68",
+        alt: "fryttt",
+        logo: "/assets/images/Customers/c68.png"
+    },        
+    {
+        id: "69",
+        alt: "markitt",
+        logo: "/assets/images/Customers/c69.png"
+    },
+    {
+        id: "70",
+        alt: "cocochan",
+        logo: "/assets/images/Customers/c70.png"
+    },        
+
+    {
+        id: "71",
+        alt: "ghalib",
+        logo: "/assets/images/Customers/c71.png"
+    },
+    {
+        id: "72",
+        alt: "bellaVita",
+        logo: "/assets/images/Customers/c72.png"
+    },        
+    {
+        id: "73",
+        alt: "delfrio",
+        logo: "/assets/images/Customers/c73.png"
+    },
+    {
+        id: "74",
+        alt: "kaybees",
+        logo: "/assets/images/Customers/c74.png"
+    },        
+    {
+        id: "75",
+        alt: "arena",
+        logo: "/assets/images/Customers/c75.png"
+    },
+    {
+        id: "76",
+        alt: "macter",
+        logo: "/assets/images/Customers/c76.png"
+    },        
+
+    {
+        id: "77",
+        alt: "JPSD",
+        logo: "/assets/images/Customers/c77.png"
+    },
+    {
+        id: "78",
+        alt: "matiariSegarMills",
+        logo: "/assets/images/Customers/c78.png"
+    },        
+    {
+        id: "79",
+        alt: "IAC",
+        logo: "/assets/images/Customers/c79.png"
+    },
+    {
+        id: "80",
+        alt: "NMC",
+        logo: "/assets/images/Customers/c80.png"
+    },        
+    {
+        id: "81",
+        alt: "libertyBooks",
+        logo: "/assets/images/Customers/c81.png"
+    },
+    {
+        id: "82",
+        alt: "al-masood",
+        logo: "/assets/images/Customers/c82.png"
+    },        
+    {
+        id: "83",
+        alt: "wah",
+        logo: "/assets/images/Customers/c83.png"
+    },
+    {
+        id: "84",
+        alt: "karachiHavaAbit",
+        logo: "/assets/images/Customers/c84.png"
+    },        
+    {
+        id: "85",
+        alt: "atco",
+        logo: "/assets/images/Customers/c85.png"
+    },
+    {
+        id: "86",
+        alt: "BinOmarSuperMarket",
+        logo: "/assets/images/Customers/c86.png"
+    },         
+]   
