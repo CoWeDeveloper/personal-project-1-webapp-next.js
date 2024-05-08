c4db7cf587112c166efa6d77b8ff8c0bb84785b9
CoWeDeveloper <owaisilyas22@gmail.com>
2024-05-08 18:58:43 +0500

    The navbar is build with icon and animation. The assets are added. In demo request page. The form UI is build.

--- src/components/Demo/Schedule.tsx
+++ src/components/Demo/Schedule.tsx
@@ -23,24 +23,10 @@ function Schedule() {
             </div>
           </div>
           <form className="">
-          <div className="relative w-full">
-            <input type="text" id="name" name="name" placeholder=" "
-             className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-400 bg-transparent rounded-lg 
-             border-2
-              border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none ng-valid ng-touched ng-dirty"/>
-              <label  
-              className="absolute text-sm text-gray-500 duration-300 transform
-               -translate-y-4 scale-75 top-2 z-10 
-              origin-[0] bg-white px-2 peer-focus:px-2 
-              peer-focus:text-gray-900 peer-placeholder-shown:scale-100 
-              peer-placeholder-shown:-translate-y-1/2 
-              peer-placeholder-shown:top-1/2 
-              peer-focus:top-2 peer-focus:scale-75 
-              peer-focus:-translate-y-4 left-1">Name</label>
-              </div>
+          
 
           <div className="flex justify-center items-center mb-4">
-            <div className="relative w-full">
+            <div className="relative w-full mr-4">
              <input type="text" id="name" name="name" placeholder=" "
              className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-400 bg-transparent rounded-lg
               border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none"/>
@@ -54,13 +40,71 @@ function Schedule() {
               peer-focus:top-2 peer-focus:scale-75
               peer-focus:-translate-y-4 left-1" >Name</label>
             </div>
-
-             <input className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none ng-pristine ng-valid ng-touched" type="text" />
+            <div className="relative w-full">
+             <input type="text" id="businessEmail" name="businessEmail" placeholder=" "
+             className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-400 bg-transparent rounded-lg
+              border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none"/>
+              <label htmlFor="businessEmail" 
+              className="absolute text-sm text-gray-500 duration-300 transform
+               -translate-y-4 scale-75 top-2 z-10 
+               origin-[0] bg-white px-2 peer-focus:px-2 
+               peer-focus:text-gray-900 peer-placeholder-shown:scale-100
+              peer-placeholder-shown:-translate-y-1/2
+               peer-placeholder-shown:top-1/2 
+              peer-focus:top-2 peer-focus:scale-75
+              peer-focus:-translate-y-4 left-1" >Business Email</label>
+            </div>
           </div>
           <div className="flex justify-center items-center mb-4">
-             <input className="block px-2.5 pb-2.5 pt-4 mr-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none ng-pristine ng-valid ng-touched" type="text" />
-             <input className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none ng-pristine ng-valid ng-touched" type="text" />
+             <div className="relative w-full mr-4">
+             <input id="country" name="country" placeholder=" " className="block px-2.5 pb-2.5 pt-4 mr-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none " type="text" />
+              <label htmlFor="country" 
+              className="absolute text-sm text-gray-500 duration-300 transform
+              -translate-y-4 scale-75 top-2 z-10 bg-white origin-[0] px-2
+              peer-focus:px-2 peer-focus:textgray-900 peer-placeholder-shown:scale-100
+              peer-placeholder-shown:-translate-y-1/2
+              peer-placeholder-shown:top-1/2
+              peer-focus:top-2 peer-focus:scale-75
+              peer-focus:-translate-y-4 left-3">Country</label>
+             </div>
+          
+             <div className="relative w-full">
+             <input id="contact" name="contact" placeholder=" " className="block px-2.5 pb-2.5 pt-4 mr-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none " type="text" />
+              <label htmlFor="contact" 
+              className="absolute text-sm text-gray-500 duration-300 transform
+              -translate-y-4 scale-75 top-2 z-10 bg-white origin-[0] px-2
+              peer-focus:px-2 peer-focus:textgray-900 peer-placeholder-shown:scale-100
+              peer-placeholder-shown:-translate-y-1/2
+              peer-placeholder-shown:top-1/2
+              peer-focus:top-2 peer-focus:scale-75
+              peer-focus:-translate-y-4 left-1">Contact No</label>
+             </div>
           </div>   
+          <div className="flex justify-center items-center mb-4">
+             <div className="relative w-full mr-4">
+             <input id="company" name="company" placeholder=" " className="block px-2.5 pb-2.5 pt-4 mr-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none " type="text" />
+              <label htmlFor="company" 
+              className="absolute text-sm text-gray-500 duration-300 transform
+              -translate-y-4 scale-75 top-2 z-10 bg-white origin-[0] px-2
+              peer-focus:px-2 peer-focus:textgray-900 peer-placeholder-shown:scale-100
+              peer-placeholder-shown:-translate-y-1/2
+              peer-placeholder-shown:top-1/2
+              peer-focus:top-2 peer-focus:scale-75
+              peer-focus:-translate-y-4 left-3">Company Name</label>
+             </div>
+          
+             <div className="relative w-full">
+             <input id="industry" name="industry" placeholder=" " className="block px-2.5 pb-2.5 pt-4 mr-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none " type="text" />
+              <label htmlFor="industry" 
+              className="absolute text-sm text-gray-500 duration-300 transform
+              -translate-y-4 scale-75 top-2 z-10 bg-white origin-[0] px-2
+              peer-focus:px-2 peer-focus:textgray-900 peer-placeholder-shown:scale-100
+              peer-placeholder-shown:-translate-y-1/2
+              peer-placeholder-shown:top-1/2
+              peer-focus:top-2 peer-focus:scale-75
+              peer-focus:-translate-y-4 left-1">Industry</label>
+             </div>
+          </div> 
           <div className="mb-4">
               <ul className="list-disc ml-4">
                 <li className="text-sm bullet">You can select multiple solutions too</li>
@@ -68,8 +112,18 @@ function Schedule() {
              <input className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none ng-pristine ng-valid ng-touched" type="text" />
           </div>
           <div >
-          <input className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none ng-pristine ng-valid ng-touched" type="text" />
-          </div>
+          <div className="relative w-full">
+             <input id="message" name="message" placeholder=" " className="block resize-none h-[190px] px-2.5 pb-2.5 pt-4 mr-4 w-full text-sm text-gray-400 bg-transparent rounded-lg border-2 border-gray-400 focus:ring-0 focus:border-gray-900 peer focus:outline-none " type="text" />
+              <label htmlFor="message" 
+              className="absolute text-sm text-gray-500 duration-300 transform
+              -translate-y-4 scale-75 top-2 z-10 bg-white origin-[0] px-2
+              peer-focus:px-2 peer-focus:textgray-900 peer-placeholder-shown:scale-100
+              peer-placeholder-shown:-translate-y-1/2
+              peer-placeholder-shown:top-7
+              peer-focus:top-2 peer-focus:scale-75
+              peer-focus:-translate-y-4 left-1">Tell us more about your business & software requirement</label>
+             </div>         
+              </div>
               <button type="submit" className="bg-[#4796cd] hover:bg-[#155777] text-sm text-white font-semibold mt-4 px-4 py-3 flex items-center rounded-lg">Submit           
                   <Image  className="ml-1" src={send} width={16} height={16} alt="laptop with screen displaying calender" />
               </button>
