aa527d4a6530ef72fcc76e19343d6e030e531b19
CoWeDeveloper <owaisilyas22@gmail.com>
2024-07-25 15:59:04 +0500

    responsive design of admin page has been successfully build only sm and xs size is remaining

--- src/components/admin/AdminPanel.tsx
+++ src/components/admin/AdminPanel.tsx
@@ -1,52 +1,59 @@
-import Image from "next/image"; 
+import Image from "next/image";
 import TableContent from "./TableContent";
 
 function AdminPanel() {
   return (
- <section className='w-full h-screen pt-24 px-14 pb-10 bg-[#f1f1f1]'>
- <header className='flex justify-between items-centre mb-14'>
-    <div className="text-stone-600 text-4xl font-semibold">Manage Posts</div>
-    <div className="space-x-4 flex items-center"> 
-    <div className="input__search">
-    <Image
-         src="./assets/icons/Admin/search.svg"
-         alt="Plus Icon"
-         width={100}
-         height={100}
-         className="w-5 mx-2"
-         />
-    <input type="text" placeholder="Search" className="outline-none w-full mx-auto"/>
+    <section className="w-full h-screen pt-24 lg:px-14 md:px-8 sm:px-0 pb-10 bg-[#f1f1f1]">
+      <header className="flex flex-wrap  md:justify-start lg:justify-between lg:items-start md:items-end sm:mb-0 md:mb-5">
+        <div className="text-stone-600 text-4xl font-semibold mb-10">
+          Manage Posts
+        </div>
+        <div className="flex lg:items-start md:items-end flex-wrap md:justify-between md:w-full lg:w-auto space-x-4">
+          <div className="input__search">
+            <Image
+              src="./assets/icons/Admin/search.svg"
+              alt="Plus Icon"
+              width={100}
+              height={100}
+              className="w-5 mx-2"
+            />
+            <input
+              type="text"
+              placeholder="Search"
+              className="outline-none w-full mx-auto"
+            />
+          </div>
+          <div className="md:flex justify-end items-end xs:flex  xs:justify-center self-end md:w-auto md:space-x-5 md:my-0 	">
 
-    </div>
-        <button className="flex items-center bg-[#58C796] hover:bg-[#57bb8f] text-white rounded-lg px-5 py-1.5">
-          
-          <Image
-          src="./assets/icons/Admin/plus.svg"
-          alt="Plus Icon"
-          width={100}
-          height={100}
-          className="w-5 mr-2"
-           />
-           Add Post
-           </button>
-           
-        <button className="flex items-center bg-[#2F7EAA] hover:bg-[#2a6a8d] text-white rounded-lg px-5 py-1.5">
-        <Image
-          src="./assets/icons/Admin/managePost.svg"
-          alt="Plus Icon"
-          width={100}
-          height={100}
-          className="w-6 mr-2"
-           />
-          Manage Post</button>
-    </div>
- </header>
+          <button className="flex sm:justify-center md:w-auto sm:w-full items-center bg-[#58C796] hover:bg-[#57bb8f] text-white rounded-lg px-5 py-1.5">
+            <Image
+              src="./assets/icons/Admin/plus.svg"
+              alt="Plus Icon"
+              width={100}
+              height={100}
+              className="w-5 mr-2"
+              />
+            Add Post
+          </button>
 
-    <TableContent />
+          <button className="flex items-center bg-[#2F7EAA] hover:bg-[#2a6a8d] text-white rounded-lg px-5 py-1.5">
+            <Image
+              src="./assets/icons/Admin/managePost.svg"
+              alt="Plus Icon"
+              width={100}
+              height={100}
+              className="w-6 mr-2"
+            />
+            Manage Post
+          </button>
+              </div>
+        </div>
+      </header>
 
-
- </section>
-  )
+      <TableContent />
+    </section>
+  );
 }
 
 export default AdminPanel;
+
--- src/components/admin/TableContent.tsx
+++ src/components/admin/TableContent.tsx
@@ -1,59 +1,94 @@
 import Image from "next/image";
 import OptionMenu from "./OptionMenu";
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 function TableContent() {
   return (
     <>
-    <Table className="border-2 border-gray-300 ">
+      <Table className="border-2 border-gray-300 ">
+        <TableHeader className="border-b-2 border-gray-300 text-stone-600 text-xl">
+          <TableRow>
+            <TableHead className="w-[50px]">S.No</TableHead>
+            <TableHead>Publish Blogs</TableHead>
+            <TableHead className="text-center">Publish Date</TableHead>
+            <TableHead className="text-right">Actions</TableHead>
+            {/* <DropdownMenuSeparator /> */}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow className="text-stone-500">
+            <TableCell className=" pl-6 font-medium ">1</TableCell>
+            <TableCell className="flex">
+              <Image
+                src="/assets/images/Blogs/b1.webp"
+                alt="Mobile innovation"
+                width={100}
+                height={100}
+                objectFit="cover"
+                className="rounded-lg w-24 mr-4"
+              />
+              <div className=" text-xl w-96 font-semibold text-stone-500 ">
+                Redefining the flexibility of media and entertainment companies
+              </div>
+            </TableCell>
+            <TableCell className="text-center font-medium">23-Jan-24</TableCell>
+            <TableCell className="text-right pr-9">
+              <OptionMenu />
+            </TableCell>
+          </TableRow>
 
-  <TableHeader className="border-b-2  border-gray-300 text-stone-600 text-xl">
-    <TableRow>
-      <TableHead className="w-[50px]">S.No</TableHead>
-      <TableHead>Publish Blogs</TableHead>
-      <TableHead className="text-center">Publish Date</TableHead>
-      <TableHead className="text-right">Actions</TableHead>
-    {/* <DropdownMenuSeparator /> */}
-    </TableRow>
-  </TableHeader>
-  <TableBody >
-    <TableRow className="text-stone-500">
-      <TableCell className=" pl-6 font-medium ">1</TableCell>
-      <TableCell className="flex">
-      <Image 
-        src="/assets/images/Blogs/b1.webp"
-        alt="Mobile innovation"
-        width={100}
-        height={100}
-        objectFit="cover"
-        className="rounded-lg w-24 mr-4"  
-        />
-        <div className=" text-xl w-96 font-semibold text-stone-500 ">
+          <TableRow className="text-stone-500">
+            <TableCell className=" pl-6 font-medium ">2</TableCell>
+            <TableCell className="flex">
+              <Image
+                src="/assets/images/Blogs/b1.webp"
+                alt="Mobile innovation"
+                width={100}
+                height={100}
+                objectFit="cover"
+                className="rounded-lg w-24 mr-4"
+              />
+              <div className=" text-xl w-96 font-semibold text-stone-500 ">
+                Redefining the flexibility of media and entertainment companies
+              </div>
+            </TableCell>
+            <TableCell className="text-center font-medium">23-Jan-24</TableCell>
+            <TableCell className="text-right pr-9">
+              <OptionMenu />
+            </TableCell>
+          </TableRow>
 
-        Redefining the flexibility of media and entertainment companies
-        </div>
-        </TableCell>
-      <TableCell className="text-center font-medium">23-Jan-24</TableCell>
-      <TableCell className="text-right pr-9">
-
-      
-      <OptionMenu />
-   
-
-
-      </TableCell>
-    </TableRow>
-  </TableBody>
-</Table>
+          <TableRow className="text-stone-500">
+            <TableCell className="pl-6 font-medium ">3</TableCell>
+            <TableCell className="flex">
+              <Image
+                src="/assets/images/Blogs/b1.webp"
+                alt="Mobile innovation"
+                width={100}
+                height={100}
+                objectFit="cover"
+                className="rounded-lg w-24 mr-4"
+              />
+              <div className=" text-xl w-96 font-semibold text-stone-500 ">
+                Redefining the flexibility of media and entertainment companies
+              </div>
+            </TableCell>
+            <TableCell className="text-center font-medium">23-Jan-24</TableCell>
+            <TableCell className="text-right pr-9">
+              <OptionMenu />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </>
-  )
+  );
 }
 
-export default TableContent
+export default TableContent;
