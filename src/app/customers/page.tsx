ba5534429c1193cf2d1df827aa43ce7030be8569
CoWeDeveloper <owaisilyas22@gmail.com>
2024-09-10 15:50:41 +0500

    vercel deploing

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -1,4 +1,6 @@
-import { HeroSection, ValuableCustomers } from '@/components/customers';
+// import { HeroSection, ValuableCustomers } from '@/components/customers';
+import HeroSection  from "@/components/customers/HeroSection";
+import ValuableCustomers from "@/components/customers/ValuableCustomers"
 
 function Customers() {
   return (
